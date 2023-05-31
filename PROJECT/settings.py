50977fd90614f06e63a1a05af85c092b5c18fa82
prosper <edwardprosper001@gmail.com>
2023-05-31 14:57:51 +0100

    'new'

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'PROJECT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+DATABASES = {     
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': '6B2-DEPLOYMENT',
+    'USER': 'postgres',
+    'PASSWORD': config('DB_PASSWORD'),
+    'HOST': 'localhost',
+    'PORT': '5432'  
+    } 
+} 
 
 
 # Password validation
